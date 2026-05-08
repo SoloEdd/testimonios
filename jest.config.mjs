c895877c98da4e0a221037372580cf63e6d5dd5c
Edd Tovar <eduardotovarlandin7723@gmail.com>
2026-05-07 21:39:25 -0600

    feat: jest added

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+// jest.config.mjs
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provee la ruta al directorio de Next.js para cargar la configuración de next.config.js y .env
+  dir: './',
+})
+
+// Configuración personalizada de Jest
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Archivo que crearemos a continuación
+}
+
+export default createJestConfig(config)
